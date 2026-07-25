317d896e5a74b2716ec30475c45d80c3d7c86629
David1396 <davichorocks1996@gmail.com>
2026-07-25 17:04:57 -0500

    feat: navbar principal

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,10 +4,10 @@ export default function Navbar() {
   return (
     <nav className="bg-blue-600 text-white p-4">
       <div className="container mx-auto flex gap-6">
-        /Inicio</Link>
-        /mascotasMascotas</Link>
-        /loginLogin</Link>
-        /registerRegistro</Link>
+        <Link href="/">Inicio</Link>
+        <Link href="/mascotas">Mascotas</Link>
+        <Link href="/login">Login</Link>
+        <Link href="/register">Registro</Link>
       </div>
     </nav>
   );
